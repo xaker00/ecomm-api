5240a0f3cce9dc9cd41204f372d95ce7022a8e68
Ilya Spivakov <ilya@spivakov.me>
2021-09-18 23:04:57 -0700

    create models

--- models/index.js
+++ models/index.js
@@ -1,16 +1,21 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+// const seedTags = require("../seeds/tag-seeds");
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: { model: "product_tag" } });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: { model: "product_tag" } });
 
 module.exports = {
   Product,
